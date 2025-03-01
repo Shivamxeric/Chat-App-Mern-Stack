e85f6b0607e09b3853772361dc0487ecb45515e1
Shivam Jha <sjhaxkira@gmail.com>
2025-03-01 12:41:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -17,7 +16,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -28,11 +26,6 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes)
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 app.get("/", (req, res) => {
 	res.send("API is running...");
